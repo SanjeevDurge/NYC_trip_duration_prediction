875088e512ea6e2f63258d986632e75be4ed2d4f
SanjeevDurge <durgesan@msu.edu>
2024-10-21 22:23:30 -0400

    Update streamlit01.py

--- cmse__nyc_project/streamlit01.py
+++ cmse__nyc_project/streamlit01.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 # import plotly.express as px
-import nbformat
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
