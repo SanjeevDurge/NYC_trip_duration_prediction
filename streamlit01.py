4ffc45333ff756d913f0a4b9ac1a54ed6f8fc495
SanjeevDurge <durgesan@msu.edu>
2024-10-21 22:47:09 -0400

    Update streamlit01.py

--- streamlit01.py
+++ streamlit01.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 import pandas as pd
-# import plotly.express as px
+import plotly.express as px
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
